1259baed1371f849217a120313b49ce19575a736
Ron Felsenfeld <ronfelsenfeld@gmail.com>
2024-02-29 10:23:29 +0200

    Add highlight to current section

--- js/gallery.controller.js
+++ js/gallery.controller.js
@@ -16,16 +16,35 @@ function renderGallery() {
       `<img src="img/${img.id}.jpg" alt="img${img.id}" class="gallery-img" onclick="onImgSelect('${img.id}')" />`
   )
 
+  showGallery()
   const elGallery = document.querySelector('.gallery')
-  elGallery.style.display = 'grid'
   elGallery.innerHTML = strHTMLs.join('')
 }
 
+////////////////////////////////////////////////////
+
+function onGallery() {
+  hideEditor()
+  hideSavedMemes()
+
+  showGallery()
+  renderGallery()
+}
+
 function onImgSelect(imgId) {
-  setImg(+imgId)
   hideGallery()
+
+  setImg(+imgId)
   showEditor()
   renderMeme()
+}
+
+function onSavedMemes() {
+  hideEditor()
+  hideGallery()
+
+  showSavedMemes()
+  renderSavedMemes()
 }
 
 function onRandomMeme() {
@@ -34,18 +53,19 @@ function onRandomMeme() {
   onImgSelect(rndIdx)
 }
 
-function onSavedMemes() {
-  hideEditor()
-  hideGallery()
-  showSavedMemes()
-  renderSavedMemes()
-}
-
 ////////////////////////////////////////////////////
 
 function hideGallery() {
   const elGallery = document.querySelector('.gallery-section')
   elGallery.style.display = 'none'
+}
+
+function showGallery() {
+  const elGallery = document.querySelector('.gallery-section')
+  elGallery.style.display = 'grid'
+
+  const elGalleryLink = document.querySelector('.btn-gallery')
+  highlightCurrSection(elGalleryLink)
 }
 
 function hideEditor() {
@@ -56,6 +76,8 @@ function hideEditor() {
 function showEditor() {
   const elEditor = document.querySelector('.editor-section')
   elEditor.style.display = 'grid'
+
+  highlightCurrSection()
 }
 
 function hideSavedMemes() {
@@ -66,8 +88,19 @@ function hideSavedMemes() {
 function showSavedMemes() {
   const elEditor = document.querySelector('.saved-memes-section')
   elEditor.style.display = 'grid'
+
+  const elSavedLink = document.querySelector('.btn-saved')
+  highlightCurrSection(elSavedLink)
 }
 
 function onToggleMenu() {
   document.body.classList.toggle('menu-open')
+}
+
+function highlightCurrSection(elBtn) {
+  const elNavBtns = [...document.querySelectorAll('.main-nav .btn')]
+  elNavBtns.forEach(btn => btn.classList.remove('selected'))
+
+  // If elBtn is undefined --> only remove highlight from links
+  if (elBtn) elBtn.classList.add('selected')
 }
